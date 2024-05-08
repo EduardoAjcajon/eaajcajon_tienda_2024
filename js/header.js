f6fa3ebb3492b0a9176b7991f8775c8853ed7167
EduardoAjcajon <eaajcajon@scl.edu.gt>
2024-05-08 10:00:34 -0600

    header

---
+++ js/header.js
@@ -0,0 +1,23 @@
+let div_header = document.querySelector("#header");
+
+// Modificar el contenido del header
+let parte_ariba = `
+    <div class="logotipo">
+        <span class="material-symbols-outlined logo"> shopping_bag </span>
+        <p class="name_tienda"> ByteVariedades </p>
+    </div>
+    <span class="material-symbols-outlined menu"> menu </span>
+    <div class="div_buscador">
+        <input type="text" name="" id="" class="buscador">
+        <div class="lupa">
+            <span class="material-symbols-outlined">
+                search
+            </span>
+        </div>
+    </div>
+    <span class="material-symbols-outlined carrito_compras"> shopping_cart </span>
+    <span class="material-symbols-outlined user" > account_circle </span>
+
+`;
+
+div_header.innerHTML = parte_ariba;
---
+++ js/main.js
@@ -0,0 +1,38 @@
+import { cargar_categorias } from "../cargar_categorias/Cargar_categorias.js";
+import { cargarProductos } from "../Cargar_productos/cargar_productos.js"
+ 
+// Selecciona el elemento con el ID 'root'
+const DOM = document.querySelector("#root");
+
+// Estructura HTML básica
+DOM.innerHTML = `
+    <header id="header"></header>
+    <main class="body">
+    <section id="div_summery_description"></section>
+    <section id="div_product_list">
+    
+    <p>Categorias</p>
+    </section>
+    </main>
+    <footer id="footer"></footer>
+`;
+
+// Función para cargar categorías y productos
+async function cargarCategoriasYProductos() {
+    try {
+        // Obtener productos de la API
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+
+        // Llamar a la función para cargar productos
+        cargarProductos(data);
+
+        // Llamar a la función para cargar categorías
+        cargar_categorias();
+    } catch (error) {
+        console.error("Error al cargar categorías y productos:", error);
+    }
+}
+
+// Llamar a la función para cargar categorías y productos
+cargarCategoriasYProductos();
